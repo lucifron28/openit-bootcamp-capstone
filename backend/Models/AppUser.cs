21f12ac9f0d7437055e79063778fd2f1e7efab5c
lucifron28 <lucifron28@gmail.com>
2026-05-25 19:55:21 +0800

    refactor: change AppUser class to inherit from IdentityUser<int> for better type safety

--- backend/Models/AppUser.cs
+++ backend/Models/AppUser.cs
@@ -2,22 +2,20 @@ using Microsoft.AspNetCore.Identity;
 
 namespace SideKick.Server.Models
 {
-  public class AppUser : IdentityUser
-  {
-    public string FirstName { get; set; } = string.Empty;
-    
-    public string LastName { get; set; } = string.Empty;
+    public class AppUser : IdentityUser<int>
+    {
+        public string FirstName { get; set; } = string.Empty;
 
-    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now.ToUniversalTime();
+        public string LastName { get; set; } = string.Empty;
 
-    public List<SocialLink> SocialLinks { get; set; } = [];
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now.ToUniversalTime();
 
-    public List<UserSkill> UserSkills { get; set; } = [];
+        public List<SocialLink> SocialLinks { get; set; } = [];
 
-    public List<GigPost> GigPosts { get; set; } = [];
+        public List<UserSkill> UserSkills { get; set; } = [];
 
-    public List<GigApplication> GigApplications { get; set; } = [];
+        public List<GigPost> GigPosts { get; set; } = [];
 
-
-  }
+        public List<GigApplication> GigApplications { get; set; } = [];
+    }
 }
