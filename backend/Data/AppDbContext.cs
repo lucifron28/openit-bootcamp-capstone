8cb46b41889fb924314a38795780c414c375fad2
lucifron28 <lucifron28@gmail.com>
2026-05-25 20:48:25 +0800

    refactor: update AppDbContext to include IdentityRole<int> and initialize DbSet properties

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -1,26 +1,28 @@
-using SideKick.Server.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SideKick.Server.Models;
 
-namespace SideKick.Server.Data {
-  public class AppDbContext : IdentityDbContext<AppUser>
-  {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+namespace SideKick.Server.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser, IdentityRole<int>, int>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<SocialLink> SocialLinks { get; set; }
+        public DbSet<SocialLink> SocialLinks { get; set; } = null!;
 
-    public DbSet<Skill> Skills { get; set; }
+        public DbSet<Skill> Skills { get; set; } = null!;
 
-    public DbSet<UserSkill> UserSkills { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; } = null!;
 
-    public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Transaction> Transactions { get; set; } = null!;
 
-    public DbSet<GigPost> GigPosts { get; set; }
+        public DbSet<GigPost> GigPosts { get; set; } = null!;
 
-    public DbSet<GigApplication> GigApplications { get; set; }
+        public DbSet<GigApplication> GigApplications { get; set; } = null!;
 
-    public DbSet<GigContract> GigContracts { get; set; }
+        public DbSet<GigContract> GigContracts { get; set; } = null!;
 
-    public DbSet<GigPayment> GigPayments { get; set; }
-  }
+        public DbSet<GigPayment> GigPayments { get; set; } = null!;
+    }
 }
