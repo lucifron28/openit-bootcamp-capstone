6037629cee93d3820daf8347dcae81d3dca56fd3
neophiles <mnv.tagle@gmail.com>
2026-05-25 22:40:40 +0800

    feat(Services): add GigPostsService

---
+++ backend/Services/GigPostsService.cs
@@ -0,0 +1,123 @@
+using Microsoft.EntityFrameworkCore;
+using SideKick.Server.Data;
+using SideKick.Server.DTOs;
+using SideKick.Server.Models;
+
+namespace SideKick.Server.Services
+{
+  public class GigPostsService : IGigPostsService 
+  {
+    private readonly AppDbContext _context;
+
+    public GigPostsService(AppDbContext context)
+    {
+      _context = context;
+    }
+
+    public List<GigPostResponseDto> GetAllGigPosts()
+    {
+      return _context.GigPosts
+        .Include(gp => gp.User)
+        .Select(gp => new GigPostResponseDto
+        {
+          Id = gp.Id,
+          Title = gp.Title,
+          Description = gp.Description,
+          CreatedAt = gp.CreatedAt,
+          UserId = gp.User!.Id,
+          Username = gp.User.UserName,
+          FirstName = gp.User.FirstName,
+          LastName = gp.User.LastName
+        })
+        .ToList();
+    }
+
+    public GigPostResponseDto CreateGigPost(string userId, PostGigPostDto newGigPost)
+    {
+      var gigPost = new GigPost
+      {
+        UserId = userId,
+        Title = newGigPost.Title,
+        Description = newGigPost.Description
+      };
+
+      _context.GigPosts.Add(gigPost);
+
+      _context.SaveChanges();
+
+      var createdPost = _context.GigPosts
+        .Include(gp => gp.User)
+        .First(gp => gp.Id == gigPost.Id);
+
+      return new GigPostResponseDto
+      {
+        Id = createdPost.Id,
+        Title = createdPost.Title,
+        Description = createdPost.Description,
+        CreatedAt = createdPost.CreatedAt,
+        UserId = userId,
+        FirstName = createdPost.User!.FirstName,
+        LastName = createdPost.User.LastName
+      };
+    }
+
+    public GigPostResponseDto? GetGigPostById(int gigPostId)
+    {
+      return _context.GigPosts
+        .Where(gp => gp.Id == gigPostId)
+        .Include(gp => gp.User)
+        .Select(gp => new GigPostResponseDto
+          {
+            Id = gp.Id,
+            Title = gp.Title,
+            Description = gp.Description,
+            CreatedAt = gp.CreatedAt,
+            UserId = gp.User!.Id,
+            FirstName = gp.User.FirstName,
+            LastName = gp.User.LastName
+          }
+        )
+        .FirstOrDefault();
+    }
+
+    public GigPostResponseDto? EditGigPostById(int gigPostId, PatchGigPostDto editedGigPost)
+    {
+      var gigPost = _context.GigPosts
+        .Include(gp => gp.User)
+        .FirstOrDefault(gp => gp.Id == gigPostId);
+
+      if (gigPost == null) return null;
+
+      if (!string.IsNullOrEmpty(editedGigPost.Title))
+      {
+        gigPost.Title = editedGigPost.Title;
+      }
+      
+      if (!string.IsNullOrEmpty(editedGigPost.Description))
+      {
+        gigPost.Description = editedGigPost.Description;
+      }
+
+      _context.SaveChanges();
+
+      return new GigPostResponseDto
+      {
+        Id = gigPost.Id,
+        Title = gigPost.Title,
+        Description = gigPost.Description,
+        CreatedAt = gigPost.CreatedAt,
+        UserId = gigPost.User!.Id,
+        FirstName = gigPost.User.FirstName,
+        LastName = gigPost.User.LastName
+      };
+    }
+
+    public void DeleteGigPostById(int gigPostId)
+    {
+      var gigPost = _context.GigPosts.FirstOrDefault(gp => gp.Id == gigPostId);
+      if (gigPost == null) return;
+      _context.Remove(gigPost);
+      _context.SaveChanges();
+    }      
+  }
+}
