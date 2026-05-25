5108095bf9f0aa4118215305f4f2154d520fce02
neophiles <mnv.tagle@gmail.com>
2026-05-25 23:07:00 +0800

    feat(Controllers): enforce authentication on gigpost routes

--- backend/Controllers/GigPostsController.cs
+++ backend/Controllers/GigPostsController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SideKick.Server.DTOs;
@@ -8,6 +9,7 @@ namespace SideKick.Server.Controllers
 {
   [ApiController]
   [Route("api/[controller]")]
+  [Authorize]
   public class GigPostsController : ControllerBase
   {
     // SERVICE
