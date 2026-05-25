a47ea7c657c1e5c6ab4dc5d4697917dcd7939dcd
lucifron28 <lucifron28@gmail.com>
2026-05-25 20:49:48 +0800

    feat: add data annotations for FirstName and LastName properties in AppUser model

--- backend/Models/AppUser.cs
+++ backend/Models/AppUser.cs
@@ -1,11 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace SideKick.Server.Models
 {
     public class AppUser : IdentityUser<int>
     {
+        [MaxLength(100)]
         public string FirstName { get; set; } = string.Empty;
 
+        [MaxLength(100)]
         public string LastName { get; set; } = string.Empty;
 
         public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now.ToUniversalTime();
