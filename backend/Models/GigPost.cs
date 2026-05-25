2a1ff05ec7c2ad5d9cfb6dbfae07110d88a8ca97
lucifron28 <lucifron28@gmail.com>
2026-05-25 20:50:26 +0800

    feat: add data annotations for Title and Description properties in GigPost model

--- backend/Models/GigPost.cs
+++ backend/Models/GigPost.cs
@@ -1,17 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SideKick.Server.Models
 {
-  public class GigPost
-  {
-    public int Id { get; set; }
+    public class GigPost
+    {
+        public int Id { get; set; }
 
-    public int UserId { get; set; }
+        public int UserId { get; set; }
 
-    public AppUser? User { get; set; }
+        public AppUser? User { get; set; }
 
-    public string Title { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(150)]
+        public string Title { get; set; } = string.Empty;
 
-    public string Description { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(2000)]
+        public string Description { get; set; } = string.Empty;
 
-    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now.ToUniversalTime();
-  }
+        public List<GigApplication> Applications { get; set; } = [];
+
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now.ToUniversalTime();
+    }
 }
