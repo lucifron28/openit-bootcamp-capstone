3033624c7a7ec757e657dad9900711170fd18463
lucifron28 <lucifron28@gmail.com>
2026-05-26 08:41:42 +0800

    fix: change userId type from string to int in ISkillsService interface methods

--- backend/Services/ISkillsService.cs
+++ backend/Services/ISkillsService.cs
@@ -5,13 +5,13 @@ namespace SideKick.Server.Services
   public interface ISkillsService
   {
     public List<SkillResponseDto> GetAllSkills();
-    public List<SkillResponseDto> GetAllSkillsOfUser(string userId);
-    public SkillResponseDto CreateSkill(string userId, PostSkillDto skill);
+    public List<SkillResponseDto> GetAllSkillsOfUser(int userId);
+    public SkillResponseDto CreateSkill(int userId, PostSkillDto skill);
     public SkillResponseDto? GetSkillById(int skillId);
     public void DeleteSkillById(int skillId);
-    public List<SkillResponseDto> GetAssignedSkillsForUser(string userId);
-    public UserSkillReponseDto? GetAssignedSkillForUserById(string userId, int skillId);
-    public UserSkillReponseDto? AssignSkillToUser(string userId, int skillId);
-    public void UnassignSkillFromUser(string userId, int skillId);
+    public List<SkillResponseDto> GetAssignedSkillsForUser(int userId);
+    public UserSkillReponseDto? GetAssignedSkillForUserById(int userId, int skillId);
+    public UserSkillReponseDto? AssignSkillToUser(int userId, int skillId);
+    public void UnassignSkillFromUser(int userId, int skillId);
   }
 }
